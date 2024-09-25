42889e64b25ea139c1d96124de691f5953584fe6
Nick Graham <nsgraham98@gmail.com>
2024-09-25 12:38:09 -0600

    added res folder

---
+++ DemoMod2/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMod2
+{
+    public class Course
+    {
+        string courseCode;
+        string courseName;
+
+        public string CourseCode { get => courseCode; set => courseCode = value; }
+        public string CourseName { get => courseName; set => courseName = value; }
+
+        public Course() { }
+
+        public Course(string courseCode, string courseName)
+        {
+            this.courseCode = courseCode;
+            this.courseName = courseName;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"Course Info:\nCourse Code: {CourseCode}\nCourse Name: {CourseName}\n";
+        }
+    }
+}
